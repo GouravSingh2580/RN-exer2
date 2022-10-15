7bfe0ce952cf8f70f3f817e1c36aaf9deff1b6d2
singh <singh@singhs-MacBook-Air.local>
2022-10-16 01:31:04 +0530

    added options in View = best feeling done

--- App.js
+++ App.js
@@ -35,10 +35,12 @@ const App = ()=>{
     <SafeAreaView style={{flex:1}}>
       <View style={styles.container}>
         <Text style={{fontWeight: "bold", fontSize:17, paddingTop:40}}>Here are some boxes of different colors</Text>
-        <Text style={[styles.a,styles.common]}>Cyan: #2aa198</Text>
-        <Text style={[styles.b,styles.common]}>Blue: #268bd2</Text>
-        <Text style={[styles.c,styles.common]}>Magenta: #d33682</Text>
-        <Text style={[styles.d,styles.common]}>Orange: #cb4b16</Text>
+        <View>
+          <Text style={[styles.a,styles.common]}>Cyan: #2aa198</Text>
+          <Text style={[styles.b,styles.common]}>Blue: #268bd2</Text>
+          <Text style={[styles.c,styles.common]}>Magenta: #d33682</Text>
+          <Text style={[styles.d,styles.common]}>Orange: #cb4b16</Text>
+        </View>
       </View>
     </SafeAreaView>
   )
